b17da5890f99d966f9f7dad66e80a6f1a943b667
Nick Braica <nick.braica@gmail.com>
2020-03-30 15:52:19 -0700

    Setup

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,37 @@
 <template>
-  <div id="app">
-    <h1>Vue / Firebase starter</h1>
-
-    <Header />
-    <router-view/>
+  <div id="app" class="app">
+    <div>
+      <h1 class="app-title">Codenames</h1>
+      <router-view/>
+    </div>
   </div>
 </template>
 
 <script>
-import Header from '@/components/Header';
 
 export default {
   name: 'App',
-  components: { Header }
 }
 </script>
+
+<style lang="scss" scoped>
+@import '@/static/styles/_abstracts.scss';
+
+.app {
+  padding: 4vw;
+  display: flex;
+  min-height: 100vh;
+  align-items: center;
+}
+
+.app-title {
+  font-weight: $fw--bold;
+  text-transform: uppercase;
+  font-size: 1rem;
+  letter-spacing: 1em;
+
+  @include mq($bp--tablet) {
+    font-size: 3vw;
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,47 @@
 <template>
-  <div class="home">
-    <h2>Home</h2>
+  <div class="lets-go">
+    <button :class="['button', {'is-loading': loading}]" @click="startGame">Start a game</button>
+    <p class="loading" v-if="loading">Loading...</p>
   </div>
 </template>
 
+<script>
+export default {
+  name: 'Home',
+  data() {
+    return {
+      loading: false
+    }
+  },
+  methods: {
+    startGame() {
+      this.loading = true;
+
+    }
+  }
+}
+</script>
+
+
+<style lang="scss" scoped>
+.lets-go {
+  margin-top: 4vh;
+  position: relative;
+}
+
+.button {
+  font-size: 2rem;
+
+  &.is-loading {
+    visibility: hidden;
+  }
+}
+
+.loading {
+  position: absolute;
+  left: 0;
+  top: 0;
+}
+
+</style>
 
